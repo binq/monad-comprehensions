2da5af73804bf006f45f01eb6629ba9714c6468a
Vanson Samuel <37637+binq@users.noreply.github.com>
2025-03-17 02:26:25 +0000

    updates

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,18 @@
 module Main where
 
-import Prelude hiding (getLine, putStr, putStrLn)
+import Prelude hiding (getLine, print, putStr, putStrLn, read)
+
 import Control.Concurrent.Async (async, waitCatch, cancel)
 import Control.Concurrent.MVar (MVar ())
-import qualified Control.Concurrent.MVar as MVar
-import Control.Monad (forever, void)
+import Control.Concurrent.MVar qualified as MVar (newEmptyMVar, putMVar, readMVar)
+import Control.Monad (forever)
 import Control.Monad.IO.Class (MonadIO (..))
 import Control.Monad.Reader (MonadReader (..), ReaderT (..), runReaderT)
-import Text.Printf (printf)
+import Text.Printf qualified as IO (printf)
+import Data.Kind (Type)
 import Data.String ()
 import Data.Text (Text ())
-import qualified Data.Text.IO as IO
+import Data.Text.IO qualified as IO (getLine, putStr, putStrLn)
 
 newEmptyMVar :: forall a {m} . MonadIO m => m (MVar a)
 newEmptyMVar = liftIO $ MVar.newEmptyMVar
@@ -30,19 +32,50 @@ putStr v = liftIO $ IO.putStr v
 putStrLn :: MonadIO m => Text -> m ()
 putStrLn v = liftIO $ IO.putStrLn v
 
-repl :: forall m c . (c ~ MVar (), MonadReader c m, MonadIO m) => m ()
-repl = void $ putStr "% " >> getLine >>= process
+said :: MonadIO m => Text -> m ()
+said v = liftIO $ IO.printf "You said: %s!\n" v
 
-process :: forall m c . (c ~ MVar (), MonadReader c m, MonadIO m) => Text -> m ()
-process "quit" = [() | _ <- ask >>= (`putMVar` ()) >> newEmptyMVar >>= readMVar]
-process l = liftIO $ printf "You said: %s!" l
+type Incoming :: Type -> Type
+data Incoming s where
+  Got :: s -> Incoming s
+  Terminate :: Incoming s
+
+type Outgoing :: Type -> Type
+data Outgoing s where
+  Report :: s -> Outgoing s
+  Pass :: Outgoing s
+
+repl :: (c ~ MVar (), MonadReader c m, MonadIO m) => m ()
+repl = forever [() | _ <- read >>= evaluate >>= print]
+
+read :: MonadIO m => m (Incoming Text)
+read = do
+  putStr "% "
+  getLine >>= \case
+    "quit" -> pure Terminate
+    v -> pure $ Got v
+
+evaluate :: (c ~ MVar (), MonadReader c m, MonadIO m) => Incoming Text -> m (Outgoing Text)
+evaluate =
+  \case
+    Terminate -> halt >> pure Pass
+    Got v -> pure $ Report v
+
+print :: MonadIO m => Outgoing Text -> m ()
+print = 
+  \case
+    Pass -> pure ()
+    Report v -> said v
+
+halt :: (c ~ MVar (), MonadReader c m, MonadIO m) => m ()
+halt = do
+  c <- ask
+  putMVar c ()
+  _ <- newEmptyMVar >>= readMVar
+  pure ()
 
 main :: IO ()
-main =
-  do
-    c <- newEmptyMVar @()
-    w <- async . forever . (`runReaderT` c) $ repl
-    readMVar c
-    cancel w
-    _ <- waitCatch w
-    putStrLn "Goodbye!"
+main = do
+  c <- newEmptyMVar @()
+  w <- async . (`runReaderT` c) $ repl
+  readMVar c >> cancel w >> waitCatch w >> putStrLn "Goodbye!"
